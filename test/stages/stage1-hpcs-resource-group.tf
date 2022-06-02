13ce961bc6e2787aab0481401d41d75155c633b3
Vamsy Sivaarcca <vamsysivaarcca@Vamsys-MacBook-Pro.local>
2022-06-01 17:08:45 -0700

    Updated stage1-hpcs-resource-group.tf file
    Closes #4
    
    Signed-off-by: Vamsy Sivaarcca <vamsysivaarcca@Vamsys-MacBook-Pro.local>

--- test/stages/stage1-hpcs-resource-group.tf
+++ test/stages/stage1-hpcs-resource-group.tf
@@ -1,7 +1,19 @@
-module "hpcs_resource_group" {
-  source = "github.com/cloud-native-toolkit/terraform-ibm-resource-group.git"
+module "dev_hpcs" {
+  source = "./module"
 
-  resource_group_name = var.hpcs_resource_group_name
-  provision           = false
-  #ibmcloud_api_key    = var.ibmcloud_api_key
+  resource_group_name    = module.resource_group.name
+  region                 = var.hpcs_region
+  name                   = var.hpcs_name
+  name_prefix            = var.name_prefix
+  provision              = false
+  number_of_crypto_units = 2
+}
+
+resource null_resource print_urls {
+  provisioner "local-exec" {
+    command = "echo -n '${module.dev_hpcs.public_url}' > .public_url"
+  }
+  provisioner "local-exec" {
+    command = "echo -n '${module.dev_hpcs.private_url}' > .private_url"
+  }
 }
